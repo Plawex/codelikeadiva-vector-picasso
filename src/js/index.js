5e003696d9e47fc25b0e3dc993034f1a3d42f132
Andreas.Arnold <andreas.arnold@diva-e.com>
2017-11-24 12:06:01 +0100

    Text added

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,37 @@
 import Project from './project';
 import ChallengeCode from './my-code/index';
+import jsonText from '../json/text.json';
+import jsonCoordinates from '../json/coordinates.json'
+
+
+class ArtworkSvg {
+    constructor() {
+        this.text = jsonText;
+        this.coordinates = jsonCoordinates;
+        this.artWorkDiv = document.getElementById("artwork-svg");
+    }
+
+    init() {
+        for (let i = 0; i < this.text.length; i += 1) {
+            const htmlTextElement = document.createElementNS("http://www.w3.org/2000/svg",'text');
+            htmlTextElement.setAttributeNS(null,'x', this.coordinates[i].x);
+            htmlTextElement.setAttributeNS(null, 'y', this.coordinates[i].y);
+            let textNode = document.createTextNode(this.text[i].text);
+            htmlTextElement.appendChild(textNode);
+            this.artWorkDiv.append(htmlTextElement);
+        }
+    }
+
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  // general project code [PLEASE KEEP THAT CODE!]
-  (new Project()).init();
+    // general project code [PLEASE KEEP THAT CODE!]
+    (new Project()).init();
 
-  // EXECUTE YOUR CODE HERE...
-  (new ChallengeCode()).init();
+    // EXECUTE YOUR CODE HERE...
+    const artworkSvg = new ArtworkSvg();
+    artworkSvg.init();
+
+    (new ChallengeCode()).init();
 });
+
