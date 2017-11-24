3cdd612463a89b5503e49b3ae910d2100fbfd6a7
Andreas.Arnold <andreas.arnold@diva-e.com>
2017-11-24 16:17:38 +0100

    img to array testing

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,54 @@
 import Project from './project';
 import ChallengeCode from './my-code/index';
 import jsonText from '../json/text.json';
-import jsonCoordinates from '../json/coordinates.json'
 
 
 class ArtworkSvg {
     constructor() {
         this.text = jsonText;
-        this.coordinates = jsonCoordinates;
         this.artWorkDiv = document.getElementById("artwork-svg");
     }
 
     init() {
-        for (let i = 0; i < this.text.length; i += 1) {
-            const htmlTextElement = document.createElementNS("http://www.w3.org/2000/svg",'text');
-            htmlTextElement.setAttributeNS(null,'x', this.coordinates[i].x);
-            htmlTextElement.setAttributeNS(null, 'y', this.coordinates[i].y);
-            let textNode = document.createTextNode(this.text[i].text);
+
+
+        let img = "?";
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+
+        ctx.drawImage(img, 0, 0);
+        var imgData = ctx.getImageData(0, 0, c.width, c.height);
+
+
+
+        let a = new Array(841);
+        for (let i = 0; i < a.length; i++) {
+            a[i] = new Array(600);
+        }
+        for (let s = 0; s < a.length; s++) {
+            for (let t = 0; t < a[s].length; t++) {
+                if (true) {
+                    a[s][t] = 1;
+                } else {
+                    a[s][t] = 0;
+                }
+            }
+        }
+        console.log(a);
+
+        for (let i = 0; i < 2000; i += 1) {
+            const htmlTextElement = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+            htmlTextElement.setAttributeNS(null, 'x', this.getRandomArbitrary(0, 842));
+            htmlTextElement.setAttributeNS(null, 'y', this.getRandomArbitrary(0, 600));
+            let c = parseInt(this.getRandomArbitrary(0, 3));
+            let textNode = document.createTextNode("I");
             htmlTextElement.appendChild(textNode);
             this.artWorkDiv.append(htmlTextElement);
         }
+    }
+
+    getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
     }
 
 }
