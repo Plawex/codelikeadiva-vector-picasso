2b6a4a4637cd63969bff02156224fff0707e02db
Andreas.Arnold <andreas.arnold@diva-e.com>
2017-11-28 20:32:39 +0100

    added instructions

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   author: "Ándreas Árnold",
   title: "Draw me like one of your front-end girls.",
-  instructions: "Bild mit ca 220px * 200px (optimal) hochladen. Einstellungen nach Belieben ändern. Reload."
+  instructions: "Bild mit ca 220px * 200px (optimal) hochladen. Einstellungen nach Belieben ändern. Reload. " +
+  "Die Pixelwerte des hochgeladenen Bildes werden ausgelesen(Canvas), manipuliert und durch Svg Elemente ersetzt."
 };
 
 export default config;
