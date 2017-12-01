386e55ea1ecf1f7ee7714381f9a75de3652a9dbf
Andreas.Arnold <andreas.arnold@diva-e.com>
2017-12-01 16:24:08 +0100

    instruction erweitert

--- src/config.js
+++ src/config.js
@@ -1,8 +1,14 @@
 const config = {
   author: "Ándreas Árnold",
   title: "Draw me like one of your front-end girls.",
-  instructions: "Bild mit ca 211px * 200px hochladen. Einstellungen nach Belieben ändern. Reload. " +
-  "Die Pixelwerte des hochgeladenen Bildes werden ausgelesen(Canvas), manipuliert und durch Svg Elemente ersetzt."
+  instructions: "Bild mit ca 211px * 200px hochladen (optimal). Einstellungen nach Belieben ändern. Reload drucken. '" +
+  "Die Pixelwerte des hochgeladenen Bildes werden ausgelesen(Canvas), manipuliert und durch Svg Elemente ersetzt. " +
+  "Optionen: " +
+  "Style: Die Pixel können durch Kreise, Rechtecke und Text ersetzt werden. Der Text kann beliebig gewählt werden. Dazu einfach ins Textfeld schreiben. " +
+  "Black-White Filter: Um so niedriger, um so weniger helle Farben werden angezeigt; Bei 255 werden alle Farben angezeigt. " +
+  "Size: Erhöht die Große der Svg Elemente; " +
+  "Distance: Erhöht den Abstand zwischen den Svg Elementen; "
+
 };
 
 export default config;
